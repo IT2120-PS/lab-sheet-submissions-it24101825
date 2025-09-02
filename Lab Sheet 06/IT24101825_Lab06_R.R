abd8797e6f97c3c420152cc7b773aba61fb0af12
it24101825 <it24101825@my.sliit.lk>
2025-09-02 19:47:34 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101825_Lab06_R.R
@@ -0,0 +1,31 @@
+setwd("C:\\Users\\IT24101825\\Desktop\\Lab06")
+dbinom(40,44,0.92)
+
+pbinom(35,44,0.92,lower.tail = TRUE)
+
+1- pbinom(37,44,0.92,lower.tail = TRUE)
+pbinom(37,44,0.92,lower.tail= FALSE)
+
+
+pbinom(42,44,0.92,lower.tail = TRUE)-pbinom(39,44,0.92,lower.tail = TRUE)
+
+
+dpois(6,5)
+
+ppois(6,5,lower.tail = FALSE)
+
+
+##Exercise##
+ 
+#part 01
+
+1- pbinom(47,50,0.85,lower.tail=TRUE)
+
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#part 02
+
+dpois(15,12)
+
+
+
